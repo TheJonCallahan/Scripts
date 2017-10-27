e1ac05090b69eb13289947ade436d833dcd5d41c
TheJonCallahan <jcallahan@live.com>
2017-10-27 19:42:22 -0400

    Added AAD BitLocker script

---
+++ Scripts and Tools/AzureMoveVHDStorageAccount.ps1
@@ -0,0 +1,51 @@
+﻿### Adapted from https://www.opsgility.com/blog/windows-azure-powershell-reference-guide/copying-vhds-blobs-between-storage-accounts/
+
+
+### Source VHD ###
+$srcURI =
+
+### Target VHD Name ###
+$destVHD =
+ 
+### Source Storage Account ###
+$srcStorageAccount =
+$srcStorageKey = 
+ 
+### Target Storage Account ###
+$destStorageAccount = 
+$destStorageKey = 
+ 
+### Create the source storage account context ### 
+$srcContext = New-AzureStorageContext  –StorageAccountName $srcStorageAccount `
+                                        -StorageAccountKey $srcStorageKey  
+ 
+### Create the destination storage account context ### 
+$destContext = New-AzureStorageContext  –StorageAccountName $destStorageAccount `
+                                        -StorageAccountKey $destStorageKey  
+ 
+### Destination Container Name ### 
+$containerName = "data"
+ 
+### Create the container on the destination ### 
+New-AzureStorageContainer -Name $containerName -Context $destContext 
+ 
+### Start the asynchronous copy - specify the source authentication with -SrcContext ### 
+$blob1 = Start-AzureStorageBlobCopy -srcURI $srcURI `
+                                    -SrcContext $srcContext `
+                                    -DestContainer $containerName `
+                                    -DestBlob $destVHD`
+                                    -DestContext $destContext
+
+### Retrieve the current status of the copy operation ###
+$status = $blob1 | Get-AzureStorageBlobCopyState 
+ 
+### Print out status ### 
+$status 
+ 
+### Loop until complete ###                                    
+While($status.Status -eq "Pending"){
+  $status = $blob1 | Get-AzureStorageBlobCopyState 
+  Start-Sleep 10
+  ### Print out status ###
+  $status
+}
---
+++ Scripts and Tools/Get-BoundaryGroupMembership.ps1
@@ -0,0 +1,12 @@
+﻿$Boundaries = Get-CMBoundaryGroup | ForEach-Object {
+    $groupName = $_.Name
+    Get-CMBoundary -BoundaryGroupId $_.GroupID | ForEach-Object { 
+        [PSCustomObject]@{
+            "Group Name" = $groupName;
+            "Boundary"   = $_.DisplayName;
+            "Value"      = $_.Value;
+        }
+    }
+}
+
+$Boundaries | Export-CSV -Path $pathCSV
